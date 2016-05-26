4a0df3eb5ba8373e18507dcab4e49f9e7e821bb2
fraggrenadekitten <camran.hansen@ucc.on.ca>
2016-05-26 12:06:25 -0400

    Update senior2_2013.java

--- senior2_2013.java
+++ senior2_2013.java
@@ -1,11 +1,11 @@
 public class senior2_2013{
 	//CCC senior problem #2 2013 Bridge Transport: url = http://cemc.uwaterloo.ca/contests/computing/2013/stage1/seniorEn.pdf
 	public static void main (String[] args){
-		int[] cars = new int[]{1,1,5000,10,40,50};
-		System.out.println(bridge(100, 6, cars));
+		int[] cars = new int[]{1,1,5000,10,40,50};//make new array
+		System.out.println(bridge(100, 6, cars));//then call the function
 	}
-	public static int bridge(int max, int n, int[]cars){
-		int t = 0;
+	public static int bridge(int max, int n, int[]cars){//static means it doesnt need to be instantiated
+		int t = 0;//this is the total
 		int z = n;
 		for (int i = 0; i<n; i++){
 			t += cars[i];
